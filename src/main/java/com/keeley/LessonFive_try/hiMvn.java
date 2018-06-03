1544dd3f0375839cf0363522e8b5d1ea0a5a2972
Administrator <Administrator@PC-201701011205>
2018-06-03 14:27:38 +0800

    commit:my_try

---
+++ src/main/java/com/keeley/LessonFive_try/hiMvn.java
@@ -0,0 +1,11 @@
+package com.keeley.LessonFive_try;
+
+import org.testng.annotations.Test;
+
+public class hiMvn {
+	@Test
+	public void hiMvn_git_jenkins() {
+		System.out.println("Git+jenkis+maven");
+	}
+
+}
